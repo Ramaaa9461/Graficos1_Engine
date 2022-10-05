7578b9648d3824ab615a6f37afe3f3b73dbadf39
Ramiro Guzman <rami.guzmi@gmail.com>
2022-10-05 03:02:38 -0300

    Revert "Model View Projection vid 18"
    This reverts commit 46038920a207a1f9db3a5b0fd98cf84c17e596da.

--- Engine_Graf/Opengl_Engine/src/Headers/Shader.h
+++ Engine_Graf/Opengl_Engine/src/Headers/Shader.h
@@ -2,8 +2,6 @@
 #include <string>
 #include <unordered_map>
 
-#include "glm/glm.hpp"
-
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -29,11 +27,11 @@ private:
 		"\n"
 		"layout(location = 1) in vec2 texCoord;"
 		"\n"
-		"out vec2 v_TexCoord;\n"
-		"uniform mat4 u_MVP;\n"
+		"out vec2 v_TexCoord;"
+		"\n"
 		"void main()\n"
 		"{\n"
-		"	gl_Position = u_MVP * position;\n"
+		"	gl_Position = position;\n"
 		"	v_TexCoord = texCoord;\n"
 		"};\n";
 
@@ -69,7 +67,6 @@ public:
 	void SetUniforms1f(const std::string name, float value);
 	void SetUniforms1i(const std::string name, int value);
 	void SetUniforms4f(const std::string name, float v0, float v1, float v2, float v3);
-	void SetUniformsMat4f(const std::string name, const glm::mat4& matrix);
 
 private:
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
